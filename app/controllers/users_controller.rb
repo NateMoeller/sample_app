cbb4040258373410bcb251689a3b64900a1a50cd
Nathan Moeller <ndmoeller@wisc.edu>
2014-04-07 13:58:00 -0500

    Finished 9.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   def new
     @user = User.new
   end
   
   def index
     @users = User.paginate(page: params[:page])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
   
   def show
@@ -43,6 +50,10 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+	
+	def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 	
 	# Before filters
