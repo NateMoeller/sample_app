5e658be8eadaa3a87c28a397a96ec86c658b726a
Nathan Moeller <ndmoeller@wisc.edu>
2014-04-07 16:59:30 -0500

    actually finished exercise 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,14 +39,18 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
+    if current_user
+	  redirect_to root_url
+	else
+	  @user = User.new(user_params)
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+	end  
   end
 
   private
